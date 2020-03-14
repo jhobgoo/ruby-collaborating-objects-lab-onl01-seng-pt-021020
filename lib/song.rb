8a6a45bb72af364bf3c0ac87ef20a24921994b34
Joel Hobgood <jhobgoo@gmail.com>
2020-03-14 21:55:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,18 +2,18 @@ class Song
 
   attr_accessor :song_name, :artist
     
-    @@all = []
+  @@all = []
     
   def initialize(song_name)
     @song_name = song_name
   end
   
-  def songs
-    @@all << self
-  end
-  
   def self.all
     @@all
+  end
+  
+  def songs
+    @@all << self
   end
   
 end
