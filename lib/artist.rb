cb9efbdb8ea23d0a3d8dc6b2c5915f95c05eb37d
Joel Hobgood <jhobgoo@gmail.com>
2020-03-14 22:17:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,40 +23,11 @@ class Artist
     end
   end
   
-   def self.all
-    @@all
-  end
-
-  def save
-    self.class.all << self
-  end
-  
-  def self.create
-    artist = self.new
-    artist.save
-    artist
-  end
-  
-  def self.new_by_name(name)
-    artist = self.new
-    artist.name = name
-    artist
-  end
-  
-  def self.create_by_name(name)
-    artist = self.create
-    artist.name = name
-    artist
-  end
-  
-  def self.find_by_name(name)
-    @@all.find.each do |artist|
-      artist.name == name
-    end
-  end
-  
   def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create_by_name(name)
+    if !(self.new(name))
+      self.new(name)
+    else
+      Artist.new
   end
   
 end
