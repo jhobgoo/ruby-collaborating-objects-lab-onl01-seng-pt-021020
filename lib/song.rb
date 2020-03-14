4179eb853f31500eeedbc632432ba1f5bcfb5280
Joel Hobgood <jhobgoo@gmail.com>
2020-03-14 23:03:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,9 @@ class Song
   end
   
   def self.new_by_filename(file_name)
-    Song.new(file_name
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
   end
   
 end
