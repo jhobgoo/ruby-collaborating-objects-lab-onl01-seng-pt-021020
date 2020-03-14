14f3919ddeb1cb4639ef05d9fdfa231dffc43a23
Joel Hobgood <jhobgoo@gmail.com>
2020-03-14 22:15:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,8 +23,28 @@ class Artist
     end
   end
   
+   def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
   def self.create_by_name(name)
-    artist = self.create
+    song = self.create
     song.name = name
     song
   end
