6452c3ca8118b854f9924d45c1825691a81b8a27
Joel Hobgood <jhobgoo@gmail.com>
2020-03-14 23:28:45 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,9 +7,7 @@ class MP3Importer
   end
 
   def files
-    @files ||= Dir.glob("#{path}/*.mp3").collect do |file_name| 
-      file_name.gsub("#{path}/", "")
-    end
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
   
   def import
